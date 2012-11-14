70f85d8942e527caaa55902808c53f3f2d25b8be
Greg Mathews <gregdmathews@gmail.com>
2012-11-14 10:11:51 -0800

    I changed it up so that any thing we type in can be run as a system command

--- main.cpp
+++ main.cpp
@@ -3,16 +3,19 @@
 
 using namespace std;
 
+int getFirstWhiteSpace(string);
+
 int main(){
-	string cmd;
+	string cmd = "";
 	char lastCMDS[9];
 	int cmdCount = 0;
-
+	int cmdPosition = 0;
 	cout << "\tA Custom Unix Shell 142\n\tCreated by: Alben Cheung\n";
 
 	while(cmd != "exit"){
-		if(cmd == "vim"){
-			system("vim");
+		if(cmd != ""){
+			system(cmd.c_str());
+			cmd = "";
 		}
 		cout << "#sh142 ->";
 		cin >> cmd;
