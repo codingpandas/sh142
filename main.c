a34f05005a41589ded283cb7ef7190fd9ff6659a
Alben Cheung <albencheung@Albens-MacBook-Pro.local>
2012-11-19 09:56:37 -0800

    converted shell to c

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#define MAXLINE 4096
+
+//Global Variables
+char cmd[100];
+
+int main(){
+
+	printf("\tComputer Engineering @ San Jose State University\n\tCreated by: Alben Cheung, Timothy Quan, Greg Mathews, David Kwong\n");
+	printf("$sh142 ->");
+	scanf("%c", &cmd);
+	return 0;
+}
